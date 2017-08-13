fc519ec2f6a8071b53c953e13276170d7d399bcb
Lorenzo Bugiani <lorenzo.bugiani@gmail.com>
2017-08-13 15:02:32 +0200

    Objectify config

---
+++ pwa-backend/src/main/java/io/growingabit/objectify/OfyServlet.java
@@ -0,0 +1,13 @@
+package io.growingabit.objectify;
+
+import javax.servlet.http.HttpServlet;
+
+public class OfyServlet extends HttpServlet {
+
+  private static final long serialVersionUID = -4243709944154393569L;
+
+  static {
+    // ObjectifyService.factory().register(aClass.class);
+  }
+}
+
