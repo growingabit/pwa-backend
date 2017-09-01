37434352434eaa6d4ea9c5b397239d92765cfadc
Lorenzo Bugiani <lorenzo.bugiani@gmail.com>
2017-09-01 22:01:38 +0200

    Pojo tests

--- pwa-backend/src/main/java/io/growingabit/backoffice/model/Invitation.java
+++ pwa-backend/src/main/java/io/growingabit/backoffice/model/Invitation.java
@@ -5,7 +5,6 @@ import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
 import com.googlecode.objectify.annotation.OnSave;
-
 import io.growingabit.app.model.BaseModel;
 import io.growingabit.app.utils.SecureStringGenerator;
 import io.growingabit.objectify.ObjectifyUtils;
@@ -35,9 +34,10 @@ public class Invitation extends BaseModel {
 
   private boolean confirmed;
 
-  public Invitation() {}
+  public Invitation() {
+  }
 
-  public Invitation(String school, String schoolClass, String schoolYear, String specialization) {
+  public Invitation(final String school, final String schoolClass, final String schoolYear, final String specialization) {
     super();
     this.school = school;
     this.schoolClass = schoolClass;
@@ -47,127 +47,63 @@ public class Invitation extends BaseModel {
   }
 
   public Long getId() {
-    return id;
+    return this.id;
   }
 
   public String getInvitationCode() {
-    return invitationCode;
+    return this.invitationCode;
   }
 
-  public void setInvitationCode(String invitationCode) {
+  public void setInvitationCode(final String invitationCode) {
     this.invitationCode = invitationCode;
   }
 
   public String getSchool() {
-    return school;
+    return this.school;
   }
 
-  public void setSchool(String school) {
+  public void setSchool(final String school) {
     this.school = school;
   }
 
   public String getSchoolClass() {
-    return schoolClass;
+    return this.schoolClass;
   }
 
-  public void setSchoolClass(String schoolClass) {
+  public void setSchoolClass(final String schoolClass) {
     this.schoolClass = schoolClass;
   }
 
   public String getSchoolYear() {
-    return schoolYear;
+    return this.schoolYear;
   }
 
-  public void setSchoolYear(String schoolYear) {
+  public void setSchoolYear(final String schoolYear) {
     this.schoolYear = schoolYear;
   }
 
   public String getSpecialization() {
-    return specialization;
+    return this.specialization;
   }
 
-  public void setSpecialization(String specialization) {
+  public void setSpecialization(final String specialization) {
     this.specialization = specialization;
   }
 
   public String getRelatedUserId() {
-    return relatedUserId;
+    return this.relatedUserId;
   }
 
-  public void setRelatedUserId(String relatedUserId) {
+  public void setRelatedUserId(final String relatedUserId) {
     this.relatedUserId = relatedUserId;
   }
 
   public boolean isConfirmed() {
-    return confirmed;
+    return this.confirmed;
   }
 
-  public void setConfirmed(boolean confirmed) {
+  public void setConfirmed(final boolean confirmed) {
     this.confirmed = confirmed;
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + (confirmed ? 1231 : 1237);
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    result = prime * result + ((invitationCode == null) ? 0 : invitationCode.hashCode());
-    result = prime * result + ((relatedUserId == null) ? 0 : relatedUserId.hashCode());
-    result = prime * result + ((school == null) ? 0 : school.hashCode());
-    result = prime * result + ((schoolClass == null) ? 0 : schoolClass.hashCode());
-    result = prime * result + ((schoolYear == null) ? 0 : schoolYear.hashCode());
-    result = prime * result + ((specialization == null) ? 0 : specialization.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Invitation other = (Invitation) obj;
-    if (confirmed != other.confirmed)
-      return false;
-    if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
-    if (invitationCode == null) {
-      if (other.invitationCode != null)
-        return false;
-    } else if (!invitationCode.equals(other.invitationCode))
-      return false;
-    if (relatedUserId == null) {
-      if (other.relatedUserId != null)
-        return false;
-    } else if (!relatedUserId.equals(other.relatedUserId))
-      return false;
-    if (school == null) {
-      if (other.school != null)
-        return false;
-    } else if (!school.equals(other.school))
-      return false;
-    if (schoolClass == null) {
-      if (other.schoolClass != null)
-        return false;
-    } else if (!schoolClass.equals(other.schoolClass))
-      return false;
-    if (schoolYear == null) {
-      if (other.schoolYear != null)
-        return false;
-    } else if (!schoolYear.equals(other.schoolYear))
-      return false;
-    if (specialization == null) {
-      if (other.specialization != null)
-        return false;
-    } else if (!specialization.equals(other.specialization))
-      return false;
-    return true;
   }
 
   @OnSave
--- pwa-backend/src/test/java/io/growingabit/backoffice/model/InvitationTest.java
+++ pwa-backend/src/test/java/io/growingabit/backoffice/model/InvitationTest.java
@@ -2,14 +2,12 @@ package io.growingabit.backoffice.model;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
-
 import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.SaveException;
-
 import io.growingabit.backoffice.dao.InvitationDao;
 import io.growingabit.testUtils.BaseDatastoreTest;
+import org.junit.Before;
+import org.junit.Test;
 
 public class InvitationTest extends BaseDatastoreTest {
 
@@ -23,47 +21,47 @@ public class InvitationTest extends BaseDatastoreTest {
 
   @Test
   public void confirmedInitValue() {
-    Invitation invitation = new Invitation("My school", "My class", "This Year", "My Spec");
-    dao.persist(invitation);
+    final Invitation invitation = new Invitation("My school", "My class", "This Year", "My Spec");
+    this.dao.persist(invitation);
     assertThat(invitation.isConfirmed()).isFalse();
   }
 
   @Test
   public void requiredFields() {
-    Invitation invitation = new Invitation("My school", "My class", "This Year", "My Spec");
-    dao.persist(invitation);
+    final Invitation invitation = new Invitation("My school", "My class", "This Year", "My Spec");
+    this.dao.persist(invitation);
     assertThat(invitation.getCreationDate()).isGreaterThan(0L);
   }
 
   @Test(expected = SaveException.class)
   public void missingSchoolRequiredField() {
-    Invitation invitation = new Invitation(null, "My class", "This Year", "My Spec");
-    dao.persist(invitation);
+    final Invitation invitation = new Invitation(null, "My class", "This Year", "My Spec");
+    this.dao.persist(invitation);
   }
 
   @Test(expected = SaveException.class)
   public void missingClassRequiredField() {
-    Invitation invitation = new Invitation("My school", null, "This Year", "My Spec");
-    dao.persist(invitation);
+    final Invitation invitation = new Invitation("My school", null, "This Year", "My Spec");
+    this.dao.persist(invitation);
   }
 
   @Test(expected = SaveException.class)
   public void missingYearRequiredField() {
-    Invitation invitation = new Invitation("My school", "My class", null, "My Spec");
-    dao.persist(invitation);
+    final Invitation invitation = new Invitation("My school", "My class", null, "My Spec");
+    this.dao.persist(invitation);
   }
 
   @Test(expected = SaveException.class)
   public void missingSpecRequiredField() {
-    Invitation invitation = new Invitation("My school", "My class", "This Year", null);
-    dao.persist(invitation);
+    final Invitation invitation = new Invitation("My school", "My class", "This Year", null);
+    this.dao.persist(invitation);
   }
 
   @Test
   public void checkInvitationCode() {
-    Invitation invitation = new Invitation("My school", "My class", "This Year", "My Spec");
-    dao.persist(invitation);
-    assertThat(invitation.getInvitationCode().length()).isEqualTo(7);;
+    final Invitation invitation = new Invitation("My school", "My class", "This Year", "My Spec");
+    this.dao.persist(invitation);
+    assertThat(invitation.getInvitationCode().length()).isEqualTo(7);
+    ;
   }
-
 }
