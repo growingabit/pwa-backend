9f6f6497292ecf2b75e7fbb4e1acd671d1b9066f
GianMarco Brescia <gianmarco.brescia@noovle.it>
2017-09-01 21:13:19 +0200

    Rimosso override inutile

--- pwa-backend/src/main/java/io/growingabit/backoffice/dao/InvitationDao.java
+++ pwa-backend/src/main/java/io/growingabit/backoffice/dao/InvitationDao.java
@@ -1,8 +1,5 @@
 package io.growingabit.backoffice.dao;
 
-import com.googlecode.objectify.Key;
-import com.googlecode.objectify.SaveException;
-
 import io.growingabit.app.dao.BaseDao;
 import io.growingabit.backoffice.model.Invitation;
 
@@ -11,10 +8,4 @@ public class InvitationDao extends BaseDao<Invitation> {
   public InvitationDao() {
     super(Invitation.class);
   }
-
-  @Override
-  public Key<Invitation> persist(Invitation entity) throws SaveException {
-    return super.persist(entity);
-  }
-
 }
