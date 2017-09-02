abadd688394bc0929f9ad8be4f01c07e20cee486
Lorenzo Bugiani <lorenzo.bugiani@gmail.com>
2017-09-02 14:33:02 +0200

    Transformed ObjectifyServlet in a more generic StartupServlet
    This is necessary because in StartupServlet now we not only have to register
    models to objectify, but also signup stages to SignupStageFactory

--- pwa-backend/src/main/java/io/growingabit/common/utils/StartupServlet.java
+++ pwa-backend/src/main/java/io/growingabit/common/utils/StartupServlet.java
@@ -1,16 +1,16 @@
-package io.growingabit.objectify;
-
-import javax.servlet.http.HttpServlet;
+package io.growingabit.common.utils;
 
 import com.googlecode.objectify.ObjectifyService;
-
+import io.growingabit.app.model.User;
 import io.growingabit.backoffice.model.Invitation;
+import javax.servlet.http.HttpServlet;
 
-public class OfyServlet extends HttpServlet {
+public class StartupServlet extends HttpServlet {
 
   private static final long serialVersionUID = -4243709944154393569L;
 
   static {
+    ObjectifyService.factory().register(User.class);
     ObjectifyService.factory().register(Invitation.class);
   }
 }
