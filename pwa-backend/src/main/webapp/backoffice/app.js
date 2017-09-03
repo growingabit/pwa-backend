f73a8ce1fcfb6cded45e9a972e51afe124500dd4
Lorenzo Bugiani <lorenzo.bugiani@gmail.com>
2017-09-03 17:02:56 +0200

    added angular environment
    this plugin is needed to manage testing parameters and production parameters.
    Is quite good, but when we move out the backoffice frontend to a separate repo
    and start using webpack, is better to use webpack.DefinePlugin

--- pwa-backend/src/main/webapp/backoffice/app.js
+++ pwa-backend/src/main/webapp/backoffice/app.js
@@ -1,53 +1,79 @@
-let app = angular.module('backofficeApp', [ 'ui.router', 'ngResource', 'auth0.auth0', 'angular-jwt' ])
+var app = angular.module('backofficeApp',
+    ['ui.router', 'ngResource', 'auth0.auth0', 'angular-jwt', 'environment']);
 
 app.config(config);
 
-config.$inject = [ '$stateProvider', '$urlRouterProvider', '$locationProvider',
-		'$httpProvider', 'angularAuth0Provider', 'jwtOptionsProvider' ];
+config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider',
+  '$httpProvider', 'angularAuth0Provider', 'jwtOptionsProvider',
+  'envServiceProvider'];
 
 function config($stateProvider, $urlRouterProvider, $locationProvider,
-		$httpProvider, angularAuth0Provider, jwtOptionsProvider) {
+    $httpProvider, angularAuth0Provider, jwtOptionsProvider,
+    envServiceProvider) {
 
-	$stateProvider.state({
-		name : 'home',
-		url : '/home',
-		component : 'home'
-	}).state({
-		name : 'invitation',
-		url : '/invitation',
-		component : 'invitation'
-	}).state({
-		name : 'callback',
-		url : '/callback',
-		component : 'callback'
-	})
+  envServiceProvider.config({
+    domains: {
+      development: ['localhost'],
+      production: ['*.appspot.com'],
+    },
+    vars: {
+      development: {
+        auth0CLientID: '32wl1L4tlptjPImhEvLIbrQSkwmAJx5s',
+        auth0Domain: 'growbit-development.eu.auth0.com',
+        auth0CallbakUrl: 'http://localhost:8888/backoffice/index.html#/callback'
+      },
+      production: {
+        auth0CLientID: '',
+        auth0Domain: '',
+        auth0CallbakUrl: ''
+      }
+    }
+  });
 
-	// Initialization for the angular-auth0 library
-	angularAuth0Provider.init({
-		clientID : AUTH0_CLIENT_ID,
-		domain : AUTH0_DOMAIN,
-		responseType : 'token id_token',
-		audience : 'https://' + AUTH0_DOMAIN + '/userinfo',
-		redirectUri : AUTH0_CALLBACK_URL,
-		scope : 'openid'
-	});
+  $stateProvider.state({
+    name: 'home',
+    url: '/home',
+    component: 'home'
+  }).state({
+    name: 'invitation',
+    url: '/invitation',
+    component: 'invitation'
+  }).state({
+    name: 'callback',
+    url: '/callback',
+    component: 'callback'
+  });
 
-	$locationProvider.hashPrefix('');
-	
-	$urlRouterProvider.otherwise('home');
+  // Initialization for the angular-auth0 library
+  angularAuth0Provider.init({
+    clientID: envServiceProvider.read('auth0CLientID'),
+    domain: envServiceProvider.read('auth0Domain'),
+    responseType: 'token id_token',
+    audience: 'https://' + envServiceProvider.read('auth0Domain') + '/userinfo',
+    redirectUri: envServiceProvider.read('auth0CallbakUrl'),
+    scope: 'openid'
+  });
 
-	jwtOptionsProvider.config({
-		tokenGetter : function() {
-			return localStorage.getItem('id_token');
-		}
-	});
+  $locationProvider.hashPrefix('');
 
-	$httpProvider.interceptors.push('jwtInterceptor');
+  $urlRouterProvider.otherwise('home');
+
+  jwtOptionsProvider.config({
+    tokenGetter: function () {
+      return localStorage.getItem('id_token');
+    }
+  });
+
+  $httpProvider.interceptors.push('jwtInterceptor');
+
+  // run the environment check, so the comprobation is made
+  // before controllers and services are built
+  envServiceProvider.check();
 }
 
 app.run(run);
-run.$inject = [ 'authService' ];
+run.$inject = ['authService'];
 
 function run(authService) {
-	authService.handleAuthentication();
+  authService.handleAuthentication();
 }
