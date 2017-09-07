adefaf1ba92beec18e9c91216dc14d5d24ff165f
Lorenzo Bugiani <lorenzo.bugiani@gmail.com>
2017-09-07 23:46:47 +0200

    addedd findByInvitationCode on InvitationDato

--- pwa-backend/src/main/java/io/growingabit/backoffice/dao/InvitationDao.java
+++ pwa-backend/src/main/java/io/growingabit/backoffice/dao/InvitationDao.java
@@ -1,5 +1,7 @@
 package io.growingabit.backoffice.dao;
 
+import com.google.common.base.Preconditions;
+import com.googlecode.objectify.ObjectifyService;
 import io.growingabit.backoffice.model.Invitation;
 import io.growingabit.common.dao.BaseDao;
 
@@ -7,5 +9,10 @@ public class InvitationDao extends BaseDao<Invitation> {
 
   public InvitationDao() {
     super(Invitation.class);
+  }
+
+  public Invitation findByInvitationCode(final String invitationCode) {
+    Preconditions.checkArgument(invitationCode != null, "invitationCode cannot be null");
+    return ObjectifyService.ofy().load().type(Invitation.class).filter("invitationCode", invitationCode).first().safe();
   }
 }
