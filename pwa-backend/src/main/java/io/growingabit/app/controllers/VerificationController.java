74ec7103870d3a265400d8b165715096a594eb1b
GianMarco Brescia <gianmarco.brescia@noovle.it>
2017-09-21 21:28:46 +0200

    Corretto path per callback di verifica. Aggiuto log per verfication link

--- pwa-backend/src/main/java/io/growingabit/app/controllers/VerificationController.java
+++ pwa-backend/src/main/java/io/growingabit/app/controllers/VerificationController.java
@@ -14,19 +14,22 @@ import org.joda.time.DateTime;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 
+import io.growingabit.app.dao.StudentBlockcertsOTPSignupStageDao;
 import io.growingabit.app.dao.StudentEmailSignupStageDao;
+import io.growingabit.app.model.StudentBlockcertsOTPSignupStage;
 import io.growingabit.app.model.StudentEmailSignupStage;
 import io.growingabit.app.model.User;
 import io.growingabit.jersey.annotations.Secured;
 
-@Path("api/v1/verify/email/{verificationCode}")
-public class VerificationEmailController {
+@Path("api/v1/verify")
+public class VerificationController {
 
-  private final static XLogger log = XLoggerFactory.getXLogger(VerificationEmailController.class);
+  private final static XLogger log = XLoggerFactory.getXLogger(VerificationController.class);
 
   @Secured
+  @Path("email/{code}")
   @GET
-  public Response verifyEmail(@Context final User currentUser, @PathParam("verificationCode") String verificationCode) {
+  public Response verifyEmail(@Context final User currentUser, @PathParam("code") String verificationCode) {
 
     log.entry(verificationCode);
 
@@ -42,6 +45,35 @@ public class VerificationEmailController {
 
       stage.setDone();
       new StudentEmailSignupStageDao().persist(stage);
+
+      return Response.ok().build();
+
+    } catch (final UnsupportedEncodingException e) {
+      log.catching(e);
+      return Response.status(HttpServletResponse.SC_BAD_REQUEST).build();
+    }
+
+  }
+
+  @Secured
+  @Path("blockcertsotp/{otp}")
+  @GET
+  public Response verifyBlockcertsOTP(@Context final User currentUser, @PathParam("otp") String otp) {
+
+    log.entry(otp);
+
+    try {
+      otp = new String(Base64.decodeBase64(otp), "utf-8");
+
+      final StudentBlockcertsOTPSignupStage stage = currentUser.getStage(StudentBlockcertsOTPSignupStage.class);
+
+      final DateTime dateTime = new DateTime();
+      if (!stage.getData().getOtp().equals(otp) || stage.getData().getTsExpiration() > dateTime.plusDays(7).getMillis()) {
+        return Response.status(HttpServletResponse.SC_FORBIDDEN).build();
+      }
+
+      stage.setDone();
+      new StudentBlockcertsOTPSignupStageDao().persist(stage);
 
       return Response.ok().build();
 
--- pwa-backend/src/main/java/io/growingabit/mail/MailService.java
+++ pwa-backend/src/main/java/io/growingabit/mail/MailService.java
@@ -8,6 +8,14 @@ import javax.mail.Transport;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.text.StrSubstitutor;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.growingabit.app.model.StudentEmailSignupStage;
+import io.growingabit.app.utils.GoogleUrlShortenerService;
+import io.growingabit.app.utils.Settings;
 
 import com.google.common.collect.ImmutableMap;
 
@@ -17,10 +25,14 @@ import io.growingabit.app.utils.Settings;
 
 public class MailService {
 
+  private static final XLogger log = XLoggerFactory.getXLogger(MailService.class);
+
   public static Message sendVerificationEmail(final StudentEmailSignupStage studentEmailSignupStage) throws UnsupportedEncodingException, MessagingException {
 
     final String verificationLink = createVerificationLink(studentEmailSignupStage);
     final String shortenLink = new GoogleUrlShortenerService().insertSafe(verificationLink);
+
+    log.info(shortenLink);
 
     final String subject = Settings.getConfig().getString("io.growingabit.mail.verifyemail.subject");
     final String htmlBody = new StrSubstitutor(ImmutableMap.of("verificationLink", shortenLink)).replace(Settings.getConfig().getString("io.growingabit.mail.verifyemail.template"));
@@ -36,7 +48,7 @@ public class MailService {
   }
 
   private static String createVerificationCode(final StudentEmailSignupStage studentEmailSignupStage) throws UnsupportedEncodingException {
-    return "/verificationemail/" + Base64.encodeBase64URLSafeString(studentEmailSignupStage.getData().getVerificationCode().getBytes("utf-8"));
+    return "/verify/email/" + Base64.encodeBase64URLSafeString(studentEmailSignupStage.getData().getVerificationCode().getBytes("utf-8"));
   }
 
 }
