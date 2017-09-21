4358926cbf645a6b18a893ebb838df498b68ffab
GianMarco Brescia <gianmarco.brescia@noovle.it>
2017-09-21 21:28:46 +0200

    Corretto path per callback di verifica. Aggiuto log per verfication link

--- pwa-backend/src/main/java/io/growingabit/mail/MailService.java
+++ pwa-backend/src/main/java/io/growingabit/mail/MailService.java
@@ -1,22 +1,32 @@
 package io.growingabit.mail;
 
-import com.google.common.collect.ImmutableMap;
-import io.growingabit.app.model.StudentEmailSignupStage;
-import io.growingabit.app.utils.GoogleUrlShortenerService;
-import io.growingabit.app.utils.Settings;
 import java.io.UnsupportedEncodingException;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Transport;
+
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.text.StrSubstitutor;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.growingabit.app.model.StudentEmailSignupStage;
+import io.growingabit.app.utils.GoogleUrlShortenerService;
+import io.growingabit.app.utils.Settings;
 
 public class MailService {
+
+  private static final XLogger log = XLoggerFactory.getXLogger(MailService.class);
 
   public static Message sendVerificationEmail(final StudentEmailSignupStage studentEmailSignupStage) throws UnsupportedEncodingException, MessagingException {
 
     final String verificationLink = createVerificationLink(studentEmailSignupStage);
     final String shortenLink = new GoogleUrlShortenerService().insertSafe(verificationLink);
+
+    log.info(shortenLink);
 
     final String subject = Settings.getConfig().getString("io.growingabit.mail.verifyemail.subject");
     final String htmlBody = new StrSubstitutor(ImmutableMap.of("verificationLink", shortenLink)).replace(Settings.getConfig().getString("io.growingabit.mail.verifyemail.template"));
@@ -32,7 +42,7 @@ public class MailService {
   }
 
   private static String createVerificationCode(final StudentEmailSignupStage studentEmailSignupStage) throws UnsupportedEncodingException {
-    return "/verificationemail/" + Base64.encodeBase64URLSafeString(studentEmailSignupStage.getData().getVerificationCode().getBytes("utf-8"));
+    return "/verify/email/" + Base64.encodeBase64URLSafeString(studentEmailSignupStage.getData().getVerificationCode().getBytes("utf-8"));
   }
 
 }
