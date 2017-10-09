ffa89f7def03b07aa9bff3fdf2b2c060342f20e7
Lorenzo Bugiani <lorenzo.bugiani@gmail.com>
2017-10-09 13:24:19 +0200

    delete backoffice frontend code

--- pwa-backend/src/main/webapp/backoffice/components/callback/callback.js
+++
@@ -1,8 +0,0 @@
-function CallbackController($state) {
-	var ctrl = this;
-}
-
-angular.module('backofficeApp').component('callback', {
-	templateUrl : '/backoffice/components/callback/callback.html',
-	controller : CallbackController
-});
--- pwa-backend/src/main/webapp/backoffice/services/invitationService.js
+++
@@ -1,8 +0,0 @@
-angular.module('backofficeApp').factory('InvitationService',
-		function($resource) {
-			return $resource('/backoffice/invitation/:id', { id: '@_id' }, {
-			    update: {
-			      method: 'PUT'
-			    }
-			  });
-		});
